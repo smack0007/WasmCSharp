8ddb7aec4bae19062706cb7ae4f623bd906f0540
smack0007 <zachary.snow@gmail.com>
2020-11-04 13:40:16 +0100

    Changed how the javascript function is invoked and passing parameter.

--- Program.cs
+++ Program.cs
@@ -5,16 +5,12 @@ namespace WasmCSharp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-
         static void WasmMain()
         {
             Console.WriteLine("Hello World from WASM!");
 
-            Runtime.InvokeJS("sayHello();");
+            var app = Runtime.GetGlobalObject("App") as JSObject;
+            app.Invoke("sayHello", "smack0007");
         }
     }
 }
